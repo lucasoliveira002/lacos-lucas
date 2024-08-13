7a9ba31f2acd8b4446a5e8d0e3053de881f7bb47
Lucas Oliveira <135052326+lucasoliveira002@users.noreply.github.com>
2024-08-13 16:13:09 -0300

    Add files via upload
    Continuação da atividade no dia 13/08

---
+++ lacos-lucas/atividade/script.js
@@ -0,0 +1,37 @@
+console.log("Script Carregado");
+
+function calcularAnos(){
+    console.log("Botão de Calcular clicando")
+
+    let popAInput= document.getElementById("popA").value.trim();
+    let taxaAInput=document.getElementById("taxaA").value.trim();
+
+    let popBInput= document.getElementById("popB").value.trim();
+    let taxaBInput=document.getElementById("taxaB").value.trim();
+
+    if(popAInput === "" || taxaAInput === "" || popBInput === "" || taxaBInput === ""){
+        alert("Por Favor, preencha todos os campos");
+        return;
+    }
+
+    let popA= parseInt(popAInput);
+    let taxaA=parseFloat(taxaAInput)/100;
+
+    let popB= parseInt(popBInput);
+    let taxaB=parseFloat(taxaBInput)/100;
+    
+    let anos=0
+
+    while(popA<popB){
+        popA *= (1+taxaA);
+        popB *= (1+taxaB);
+        anos++;
+    }
+
+    console.log("Anos calculados", anos);
+
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML=`Serão necessários ${anos} anos para que a população de A ultrapasse ou iguale a de B`;
+
+
+}
